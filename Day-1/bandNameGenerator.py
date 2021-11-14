46b1db09a117c3a9f6ab7b197d8e08a87e573eb9
Nischal Newar <nnewar@expertinstitute.com>
2021-11-14 22:47:23 +0530

    day-3 coding challenge

---
+++ Day-1/bandNameGenerator.py
@@ -0,0 +1,12 @@
+# Day-1 task is to create a band name generator
+
+print('##### Welcome to the band name genertor program #####','\n')
+
+city_name = input("What's name of the city you grew up in?\n")
+
+pet_name = input('Please provide your pet name:\n')
+
+band_name = city_name + ' ' + pet_name
+
+print('Your band name is:')
+print(band_name)
