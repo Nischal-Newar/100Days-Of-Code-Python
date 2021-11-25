df92299bd91ff7448deacfbbc1266c81edc79e9d
Nischal Newar <nnewar@expertinstitute.com>
2021-11-25 22:44:56 +0530

    added the prime number challenge

---
+++ Day-8/prime_number_checker.py
@@ -0,0 +1,16 @@
+#### Check if a numer is prime number ####
+# function to check prime number
+def prime_checker(number):
+    flag = False
+    if number > 1:
+        for i in range(2, number):
+            if number % i == 0:
+                flag = True
+                break
+    if flag:
+        print(number, "is not a prime number")
+    else:
+        print(number, "is a prime number")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
