ce9c96ed7e08d180ccbe66ece16371ce3338a9a1
Nischal Newar <nnewar@expertinstitute.com>
2021-12-20 21:53:32 +0530

    Day-17

---
+++ Day-17/quiz_game/question_model.py
@@ -0,0 +1,7 @@
+# created class Question to declare text and answer attributes
+class Question:
+    def __init__(self, question_text, question_answer):
+        self.text = question_text
+        self.answer = question_answer
+
+
